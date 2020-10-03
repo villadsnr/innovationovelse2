6969567bbb6741055978d4b22ef585084879a54d
villadsrasmussen <villadsnr@outlook.dk>
2020-10-03 12:20:22 +0200

    test2

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+/*
+TEEEEEST
+ */
+
 export default function App() {
   return (
     <View style={styles.container}>
